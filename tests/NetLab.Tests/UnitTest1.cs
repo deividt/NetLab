46b2b14b9243c7329cc0b057e12bcc8623d5e81a
Deividt gemeli <deividt@gmail.com>
2022-11-01 22:45:04 -0300

    Added Hello World unit test example

--- tests/NetLab.Tests/UnitTest1.cs
+++ tests/NetLab.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using NetLab.Library;
+
 namespace NetLab.Tests;
 
 public class UnitTest1
@@ -5,5 +8,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Arrange
+        var sut = new Class1();
+
+        // Act
+        var result = sut.HelloWorld();
+
+        // Assert
+        result.Should().Be("Hello World");
     }
 }
